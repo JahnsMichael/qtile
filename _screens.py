c37794c0373388b968c2cdddb8ea4ea4cdc0a204
jahnsmichael <michael.jf.jm@gmail.com>
2021-05-04 12:58:34 +0700

    restyling the bar

--- _screens.py
+++ _screens.py
@@ -19,7 +19,7 @@ def get_default_screen():
                 widget.GroupBox(
                     rounded=False,
                     highlight_method="line",
-                    highlight_color= [colors.common['bg'], colors.brown[2]],
+                    highlight_color=[colors.common['bg'], colors.brown[2]],
                     this_screen_border=colors.common['accent'],
                     this_current_screen_border=colors.brown[2],
                     other_current_screen_border=colors.common['ui'],
@@ -37,26 +37,30 @@ def get_default_screen():
                 widget.Chord(),
                 get_sep(),
                 widget.Memory(
-                    foreground=colors.blue[1],
-                    format='{MemPercent}%{MemUsed: .0f}M/{MemTotal: .0f}M'
+                    foreground=colors.blue[0],
+                    format='{MemPercent}%{MemUsed: .0f}M/{MemTotal: .0f}M',
+                    font='Victor Mono Bold'
                 ),
                 get_sep(),
                 widget.Systray(foreground=colors.brown[5]),
                 get_sep(),
                 widget.Clock(
                     format='%a, %d %b %Y | %H:%M',
-                    foreground=colors.green[1],
+                    foreground=colors.green[0],
+                    font='Victor Mono Bold'
                 ),
                 get_sep(),
                 widget.QuickExit(
                     default_text='⏻',
-                    foreground=colors.red[2],
+                    foreground=colors.red[0],
+                    font='Victor Mono Bold'    
                 ),
                 get_sep(),
             ],
             24,
         )
     )
+
 
 def get_screens():
     return [get_default_screen() for i in range(len(get_monitors()))]
