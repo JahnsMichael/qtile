26c2bf340db1385c82e406d70b5c6066d8fc5566
jahnsmichael <michael.jf.jm@gmail.com>
2021-06-24 13:06:58 +0700

    add key binding to toggle bar view

--- lib/keys/controls.py
+++ lib/keys/controls.py
@@ -1,6 +1,7 @@
 from libqtile.config import Key
 from libqtile.lazy import lazy
 
+
 CONTROL_KEYS = [
     Key(["control"], "r", lazy.restart(), desc="Restart Qtile"),
     Key(["control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
@@ -8,4 +9,8 @@ CONTROL_KEYS = [
         desc="Lock Screen with betterlockscreen"),
     Key(["control"], "s", lazy.spawn(["betterlockscreen", "-s"]),
         desc="Suspend with betterlockscreen"),
+    Key(["control"], "Home", lazy.hide_show_bar(position="top"),
+        desc="Toggle top bar"),
+    Key(["control"], "End", lazy.hide_show_bar(position="bottom"),
+        desc="Toggle bottom bar"),
 ]
