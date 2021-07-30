5f8534d1204b4472b2460845dbe929203ecc234e
jahnsmichael <michael.jf.jm@gmail.com>
2021-07-30 11:32:07 +0700

    add new mouse action

--- lib/mouse/__init__.py
+++ lib/mouse/__init__.py
@@ -4,9 +4,13 @@ from lib.const import MOD
 
 def get_mouse():
     return [
+        Click([MOD], "Button1", lazy.window.bring_to_front()),
         Drag([MOD], "Button1", lazy.window.set_position_floating(),
              start=lazy.window.get_position()),
+        Drag([MOD, "shift"], "Button1", lazy.window.set_size_floating(),
+             start=lazy.window.get_size()),
         Drag([MOD], "Button3", lazy.window.set_size_floating(),
              start=lazy.window.get_size()),
-        Click([MOD], "Button2", lazy.window.bring_to_front())
+        Click([MOD], "Button2", lazy.window.toggle_floating()),
+        Click([MOD, "control"], "Button2", lazy.window.kill())
     ]
